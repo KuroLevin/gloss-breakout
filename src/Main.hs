62dae70bb9231e4fc25c7c8f4665a5e61920678c
KuroLevin <pedromoreno94@gmail.com>
2018-03-20 23:40:12 +0000

    added random generation of types of blocks and their effects

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
-
 module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import System.Random
 
 maxWidth, maxHight :: Float
 maxWidth = 640
@@ -11,7 +11,7 @@ maxHight = 480
 type Block = (Point, -- center
               Float, -- width
               Float, -- hight
-              Color)
+              Int)
 
 type Pad = (Float, --position
             Float, --speed
@@ -21,13 +21,14 @@ type Pad = (Float, --position
 type Ball = (Point,  --center
              Vector, --speed
              Float,  --size
-             Color)
+             Int)    --bonus
 
 
-data Shape = Block Block
-           | Pad Pad
-           | Ball Ball
-           deriving(Show)
+data Shapes = Shapes
+        [Pad]
+        [Ball]
+        [Block]
+        deriving(Show)
 
 
 window :: Display
@@ -35,6 +36,9 @@ window = InWindow "Breakout" (round maxWidth,round maxHight) (0,0)
 
 fps :: Int
 fps = 60
+
+colors :: [Color]
+colors = [yellow, red, green, blue]
 
 -- Blocks
 
@@ -46,7 +50,7 @@ blockLines = 10
 blockColumns = 10
 
 genBlock :: Point ->  Block
-genBlock (x,y) = ((x,y), maxWidth/blockColumns-1, blockAreaSize/blockLines-1, red)
+genBlock (x,y) = ((x,y), maxWidth/blockColumns-1, blockAreaSize/blockLines-1, 0)
 
 genBlocksLine :: Float -> [Block]
 genBlocksLine n = map f [-(blockColumns-1)/2..(blockColumns-1)/2] where
@@ -56,8 +60,13 @@ genBlocksLine n = map f [-(blockColumns-1)/2..(blockColumns-1)/2] where
 genBlocksGrid :: [Block]
 genBlocksGrid = concat(map genBlocksLine [1..blockLines])
 
+randomizeBlocks :: (RandomGen g) => [Block] -> g -> [Block]
+randomizeBlocks [] _ = []
+randomizeBlocks ((p,w,h,_):xs) g = (p,w,h,t):(randomizeBlocks xs gn) where
+    (t,gn) = randomR (0,(length colors)-1) g 
+
 drawBlock :: Block -> Picture
-drawBlock ((x,y),w,h,c) = color c (translate x y (rectangleSolid w h))
+drawBlock ((x,y),w,h,c) = color (colors!!c) (translate x y (rectangleSolid w h))
 
 -- Pad
 
@@ -65,7 +74,7 @@ padSize :: Float
 padSize = maxWidth/10
 
 padSpeed :: Float
-padSpeed = 100
+padSpeed = 200
 
 genPad :: Pad
 genPad = (0, 0, padSize, white)
@@ -73,8 +82,8 @@ genPad = (0, 0, padSize, white)
 drawPad :: Pad -> Picture
 drawPad (p, v, s, c) = color c (translate p (-maxHight/2+1) (line [(-s/2, 0),(s/2,0)]))
 
-updatePad :: Float -> Shape -> Shape
-updatePad dt (Pad (p, v, s, c)) = Pad ((max (min (maxWidth/2) (p+v*dt)) (-maxWidth/2)), v, s, c)
+updatePad :: Float -> Pad -> Pad
+updatePad dt (p, v, s, c) = ((max (min (maxWidth/2) (p+v*dt)) (-maxWidth/2)), v, s, c)
 
 -- Ball
 
@@ -82,64 +91,57 @@ ballSize :: Float
 ballSize = padSize/10
 
 ballPosition :: Point
-ballPosition = (0, (-maxHight)/4)
+ballPosition = (0, (-maxHight)/8)
 
 ballSpeed :: Vector
-ballSpeed = (50, -50)
+ballSpeed = (100, -100)
 
 genBall :: Ball
-genBall = (ballPosition, ballSpeed, ballSize, white)
+genBall = (ballPosition, ballSpeed, ballSize, 0)
 
 drawBall :: Ball -> Picture
-drawBall ((x,y),v,s,c) = color c (translate x y (circleSolid s))
+drawBall ((x,y),v,s,c) = color (colors!!c) (translate x y (circleSolid s))
 
+updateBall :: Float -> Ball -> Ball
+updateBall dt ((x,y), (dx, dy), s, c) = ((x+dt*dx,y+dt*dy), (dx, dy), s, c)
 
-updateBall :: Float -> Shape -> Shape
-updateBall dt (Ball ((x,y), (dx, dy), s, c)) = Ball ((x+dt*dx,y+dt*dy), (dx, dy), s, c)
-
+duplicateBalls :: [Ball] -> [Ball]
+duplicateBalls [] = []
+duplicateBalls ((p,(dx,dy),s,b):xs) = if b >= 4 
+    then (p,(-dx,dy),s,0):(p,(dx,-dy),s,0):(duplicateBalls xs)
+    else (p,(dx,dy),s,b):(duplicateBalls xs)
 
 --Shapes
 
-drawShape :: Shape -> Picture
-drawShape (Block b) = drawBlock b
-drawShape (Pad p) = drawPad p
-drawShape (Ball b) = drawBall b
+genAllShapes :: (RandomGen g) => g -> Shapes
+genAllShapes g = Shapes [genPad] [genBall] (randomizeBlocks genBlocksGrid g)
 
-genAllShapes :: [Shape]
-genAllShapes = Pad genPad : Ball genBall : map Block genBlocksGrid
-
-isBall :: Shape -> Bool
-isBall (Ball _) = True
-isBall _ = False
-
-isPad :: Shape -> Bool
-isPad (Pad _) = True
-isPad _ = False
-
-isBlock :: Shape -> Bool
-isBlock (Block _) = True
-isBlock _ = False
-
-colisionsBlockBall :: Float -> Shape -> Shape -> Bool
-colisionsBlockBall dt (Block ((x2,y2),w,h,_)) (Ball ((x1,y1),(dx,dy),s,_))
+colisionsBlockBall :: Float -> Block -> Ball -> Bool
+colisionsBlockBall dt ((x2,y2),w,h,_) ((x1,y1),(dx,dy),s,_)
     | abs(x1+dt*dx-x2) <= w/2+s && abs(y1+dt*dy-y2) <= h/2+s = True
     | otherwise = False
 
-colisionsBallWall :: Float -> Shape -> Shape
-colisionsBallWall dt (Ball ((x,y),(dx,dy),s,c))
-    | x+dt*dx+s > maxWidth/2 || x+dt*dx-s < -maxWidth/2 = Ball ((x,y),(-dx,dy),s,c)
-    | y+dt*dy+s > maxHight/2 = Ball ((x,y),(dx,-dy),s,c)
-    | otherwise = Ball ((x,y),(dx,dy),s,c)
+colisionsBallWall :: Float -> Ball -> Ball
+colisionsBallWall dt ((x,y),(dx,dy),s,c)
+    | x+dt*dx+s > maxWidth/2 || x+dt*dx-s < -maxWidth/2 = ((x,y),(-dx,dy),s,c)
+    | y+dt*dy+s > maxHight/2                            = ((x,y),(dx,-dy),s,c)
+    | otherwise                                         = ((x,y),(dx,dy),s,c)
 
-colisionsBallBlock :: Float -> Shape -> Shape -> Shape
-colisionsBallBlock dt (Ball ((x1,y1), (dx,dy), s, c)) (Block ((x2,y2), w, h, _))
-    | abs(x1+dt*dx-x2) > w/2+s || abs(y1+dt*dy-y2) > h/2+s = Ball ((x1,y1),(dx,dy),s,c)
-    | abs(x1-x2) > w/2+s && abs(x1+dt*dx-x2) <= w/2+s      = Ball ((x1,y1),(-dx,dy),s,c)
-    | abs(y1-y2) > h/2+s && abs(y1+dt*dy-y2) <= h/2+s      = Ball ((x1,y1),(dx,-dy),s,c)
-    | otherwise                                            = Ball ((x1,y1),(dx,dy),s,c)
+colisionsBallBlock :: Float -> Ball -> Block -> Ball
+colisionsBallBlock dt ((x1,y1), (dx,dy), s, c) ((x2,y2), w, h, t)
+    | abs(x1+dt*dx-x2) > w/2+s || abs(y1+dt*dy-y2) > h/2+s    = ((x1,y1),(dx,dy),s,c)
+    | abs(x1-x2) > w/2+s && abs(x1+dt*dx-x2) <= w/2+s && t==1 = ((x1,y1),(-dx*1.2,dy*1.2),s,c)
+    | abs(y1-y2) > h/2+s && abs(y1+dt*dy-y2) <= h/2+s && t==1 = ((x1,y1),(dx*1.2,-dy*1.2),s,c)
+    | abs(x1-x2) > w/2+s && abs(x1+dt*dx-x2) <= w/2+s && t==2 = ((x1,y1),(-dx/1.2,dy/1.2),s,c)
+    | abs(y1-y2) > h/2+s && abs(y1+dt*dy-y2) <= h/2+s && t==2 = ((x1,y1),(dx/1.2,-dy/1.2),s,c)
+    | abs(x1-x2) > w/2+s && abs(x1+dt*dx-x2) <= w/2+s && t==3 = ((x1,y1),(-dx,dy),s,c+1)
+    | abs(y1-y2) > h/2+s && abs(y1+dt*dy-y2) <= h/2+s && t==3 = ((x1,y1),(dx,-dy),s,c+1)
+    | abs(x1-x2) > w/2+s && abs(x1+dt*dx-x2) <= w/2+s         = ((x1,y1),(-dx,dy),s,c)
+    | abs(y1-y2) > h/2+s && abs(y1+dt*dy-y2) <= h/2+s         = ((x1,y1),(dx,-dy),s,c)
+    | otherwise                                               = ((x1,y1),(dx,dy),s,c)
 
-colisionsBallVoid :: Float -> Shape -> Shape -> Bool
-colisionsBallVoid dt (Ball ((x,y),(dx,dy),s1,_)) (Pad (p,v,s2,_))
+colisionsBallVoid :: Float -> Ball -> Pad -> Bool
+colisionsBallVoid dt ((x,y),(dx,dy),s1,_) (p,v,s2,_)
     | y+dt*dy <= s1-maxHight/2 && abs(x+dt*dx-(p+dt*v)) >= s2 = True
     | otherwise                                               = False
 
@@ -147,47 +149,46 @@ padBounce :: Float -> Vector -> Vector
 padBounce pos (dx,dy) = (v*sin(pos*pi/2), v*cos(pos*pi/2)) where
     v = sqrt(dx**2+dy**2)
 
-colisionsBallPad :: Float -> Shape -> Shape -> Shape
-colisionsBallPad dt (Ball ((x,y),(dx,dy),s1,c)) (Pad (p, v, s2, _))
-    | y+dt*dy > s1-maxHight/2        = Ball ((x,y),(dx,dy),s1,c)
-    | abs(x+dt*dx-p-dt*v) < s2 = Ball ((x,y),(padBounce ((x+dt*dx-p-dt*v)/s2) (dx,dy)),s1,c)
+colisionsBallPad :: Float -> Ball -> Pad -> Ball
+colisionsBallPad dt ((x,y),(dx,dy),s1,c) (p, v, s2, _)
+    | y+dt*dy > s1-maxHight/2  = ((x,y),(dx,dy),s1,c)
+    | abs(x+dt*dx-p-dt*v) < s2 = ((x,y),(padBounce ((x+dt*dx-p-dt*v)/s2) (dx,dy)),s1,c)
 
 
-update :: Float -> [Shape] -> [Shape]
-update dt shapes = pads ++ balls ++ blocks where
-    allPads = filter isPad shapes
-    allBalls = filter isBall shapes
-    allBlocks = filter isBlock shapes
-    balls1 = [ colisionsBallWall dt ball 
-             | ball<-allBalls]
-    balls2 = [ colisionsBallPad dt ball pad 
-             | ball<-balls1, pad<-allPads, not (colisionsBallVoid dt ball pad)]
-    balls3 = [ foldl (colisionsBallBlock dt) ball allBlocks 
-             | ball<-balls2]
-    balls = [ updateBall dt ball 
-            | ball<-balls3]
+update :: Float -> Shapes -> Shapes
+update dt (Shapes allPads allBalls allBlocks) = Shapes pads balls blocks where
+    balls1 = [ updateBall dt (foldl 
+                                (colisionsBallBlock dt) 
+                                (foldl 
+                                    (colisionsBallPad dt) 
+                                    (colisionsBallWall dt ball) 
+                                    allPads) 
+                                allBlocks)
+            | ball<-allBalls, any (not . colisionsBallVoid dt ball) allPads]
+    balls = duplicateBalls balls1
     blocks = [ block 
              | block<-allBlocks, not (any (colisionsBlockBall dt block) allBalls)]
     pads = [ updatePad dt pad 
            | pad<-allPads] 
 
 
-render :: [Shape] -> Picture
-render shapes = pictures (map drawShape shapes)
+render :: Shapes -> Picture
+render (Shapes pads balls blocks) = pictures ((map drawPad pads) ++ (map drawBall balls) ++ (map drawBlock blocks))
 
 
-
-react :: Event -> [Shape] -> [Shape]
-react event [] = []
-react (EventKey (SpecialKey KeyLeft) Down _ _) ((Pad (p, v, s, c)):xs) = (Pad (p, -padSpeed, s, c)):xs
-react (EventKey (SpecialKey KeyRight) Down _ _) ((Pad (p, v, s, c)):xs) = (Pad (p, padSpeed, s, c)):xs
-react _ ((Pad (p, v, s, c)):xs) = (Pad (p, 0, s, c)):xs
-react event (x:xs) = x : (react event xs)
-
+react :: Event -> Shapes -> Shapes
+react event (Shapes [] balls blocks) = Shapes [] balls blocks
+react (EventKey (SpecialKey KeyLeft) Down _ _) (Shapes ((p, v, s, c):xs) balls blocks) =
+    Shapes ((p, v-padSpeed, s, c):xs) balls blocks
+react (EventKey (SpecialKey KeyRight) Down _ _) (Shapes ((p, v, s, c):xs) balls blocks) =
+    Shapes ((p, v+padSpeed, s, c):xs) balls blocks
+react (EventKey (SpecialKey KeyLeft) Up _ _) (Shapes ((p, v, s, c):xs) balls blocks) =
+    Shapes ((p, v+padSpeed, s, c):xs) balls blocks
+react (EventKey (SpecialKey KeyRight) Up _ _) (Shapes ((p, v, s, c):xs) balls blocks) =
+    Shapes ((p, v-padSpeed, s, c):xs) balls blocks
+react _ x = x
 
 
 main :: IO ()
-main = do 
-    play window black fps genAllShapes render react update
-
-
+main = do
+    play window black fps (genAllShapes (mkStdGen 0)) render react update
